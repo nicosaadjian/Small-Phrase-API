62b1dd27780fb9df610a7e1ad8b4869a5ac1f2c3
nicosaadjian <nicosaadjian@gmail.com>
2025-09-17 21:44:33 -0300

    update test

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ async function postPhrases(friendName, phrase){
     db.none('INSERT INTO frases(friendName, phrase) VALUES($1, $2)', [friendName, phrase]);
 }
 
+console.log("Si estoy corriendo app.js ok en AWS EC2 desde Ubunbtu, a continuacion se imprimira LA HORA:\n");
+console.log(db.any("SELECT NOW();"));
